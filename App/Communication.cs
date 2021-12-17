176673659e76832bd40c349c2ca72a571d6818f2
junjie <2919439744zjj@gmail.com>
2021-12-17 10:08:52 +0900

    원래 있는 error를 다 수정하고 Relay 상태값을 가져오는 부분도 추가

---
+++ App/Communication.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace usbrelay
+{
+    public sealed class Communication
+    {
+
+        SerialPort sp = new SerialPort();
+        private static Communication communication;
+
+        private Communication()
+        {
+
+        }
+
+
+        public static Communication Instance()
+        {
+            if (communication == null)
+            {
+                communication = new Communication();
+            }
+
+            return communication;
+        }
+
+
+
+        public void Communication_Set(int baudrate, int databits)
+        {
+            sp.BaudRate = baudrate;
+            sp.DataBits = databits;
+            sp.ReadTimeout = 500;
+            sp.WriteTimeout = 500;
+        }
+
+        public string[] selectCOM()
+        {
+            return SerialPort.GetPortNames();
+        }
+
+        public string openCOM(string combosel)
+        {
+            if (!sp.IsOpen)
+            {
+
+                sp.PortName = combosel;
+                try
+                {
+                    sp.Open();
+                    return "The port has been opened!";
+                }
+
+                catch
+                {
+                    return "Can not open the port!";
+                }
+            }
+
+            else
+            {
+                return "The port is already opened!";
+            }
+        }
+
+        public string CloseCOM()
+        {
+            if (sp.IsOpen)
+            {
+                try
+                {
+                    sp.Close();
+                    return "The port has been closed";
+                }
+
+                catch
+                {
+                    return "The port can not be closed";
+                }
+            }
+
+            else
+            {
+                return "The port is already closed!";
+            }
+        }
+
+
+
+        public bool serialWrite(byte[] dataBuff, int offset, int count)
+        {
+            try
+            {
+                if (sp.IsOpen) { sp.Write(dataBuff, offset, count); return true; }
+                else { return false; }
+            }
+
+            catch
+            {
+                return false;
+            }
+        }
+
+        public byte serialRead()
+        {
+            return (byte)sp.ReadByte();
+        }
+
+        /*public int serialBytesToRead()
+        {
+            return sp.BytesToRead;
+        }*/
+
+
+        public bool serialIsOpen()
+        {
+            if (sp.IsOpen) return true;
+            else return false;
+        }
+
+    }
+}
--- App/MainWindow.xaml.cs
+++ App/MainWindow.xaml.cs
@@ -14,7 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Forms;
 using System.Windows.Forms.Integration;
-
+using System.Threading;
+using System.IO.Ports;
 /**
  * KMtronic Usb 1ch relay control example
  */
@@ -26,63 +27,144 @@ namespace usbrelay
     public partial class MainWindow : Window
     {
         private System.IO.Ports.SerialPort serialPort1;
+        Communication comm;
+        
 
         public MainWindow()
         {
             this.serialPort1 = new System.IO.Ports.SerialPort(new System.ComponentModel.Container());
             InitializeComponent();
+            comm = Communication.Instance();
+            comm.Communication_Set(9600, 8);
+            
             panel1.BackColor = Color.Red;
             label3.Content = "Please select COM port";
         }
 
+        
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             comboBox1.Items.Clear();
             foreach (string com in System.IO.Ports.SerialPort.GetPortNames()) 
             comboBox1.Items.Add(com);
         }
+
         
         private void ConnectUsb(object sender, RoutedEventArgs e)
         {
-            if (serialPort1.IsOpen)
+            if (!serialPort1.IsOpen)
             {
-                serialPort1.Close();
-                serialPort1.PortName = comboBox1.Items[comboBox1.SelectedIndex].ToString();
+                // byte[] rxByte = new byte[3];
+                try
+                {
+                    serialPort1.PortName = comboBox1.Items[comboBox1.SelectedIndex].ToString();
+                    serialPort1.Open();
+                    //Thread.Sleep(50);\
+                    serialPort1.Write(new byte[] { 0xFF, 0x01, 0x03 }, 0, 3);
+                    int result1 = serialPort1.ReadByte();
+                    int result2 = serialPort1.ReadByte();
+                    int result3 = serialPort1.ReadByte();
+                    Console.WriteLine("0 ： " + result1);
+                    Console.WriteLine("1 ： " + result2);
+                    Console.WriteLine("2 ： " + result3);
+                    if (result3==1)
+                    {
+                        // ON
+                        label4.Content = "Relay State is ON!";
+                        panel2.BackColor = Color.Lime;
+
+                    } else
+                    {
+                        // OFF
+                        label4.Content = "Relay State is OFF!";
+                        panel2.BackColor = Color.Red;
+                    }
+                    label1.Content = "The port has been opened!";
+                }
+
+                catch
+                {
+                    label1.Content = "First select COM port!";
+                }
             }
+
             else
             {
-                serialPort1.PortName = comboBox1.Items[comboBox1.SelectedIndex].ToString();
-                serialPort1.Open();
-                if (serialPort1.IsOpen)
-                {
-                    label1.Content = "Port is opened successfully!";
-                }
-                else
-                {
-                    label1.Content = "Impossible to open port!";
-                }
+                 label1.Content = "The port is already opened!";
             }
+            
+            
         }
 
+       
         private void DisconnectUsb(object sender, RoutedEventArgs e)
         {
-            serialPort1.Close();
-            label1.Content = "Port successfully closed!";
+            if (serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.Close();
+                    label1.Content = "Port successfully closed!";
+                }
+
+                catch
+                {
+                    label1.Content = "The port can not be closed";
+                }
+            }
+
+            else
+            {
+                label1.Content = "The port is already closed!";
+            }
+
         }
 
-        private void RelayOpen(object sender, RoutedEventArgs e)
+         private void RelayOpen(object sender, RoutedEventArgs e)
         {
-            serialPort1.Write(new byte[] { 0xFF, 0x01, 0x01 }, 0, 3);
-            panel1.BackColor = Color.Lime;
-            label2.Content = "Relay is opened successfully!";
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Write(new byte[] { 0xFF, 0x01, 0x01 }, 0, 3);
+                panel1.BackColor = Color.Lime;
+                label2.Content = "Relay is opened successfully!";
+            }
+
+            else
+            {
+                label2.Content = "The port is already closed!";
+            }
+
         }
 
         private void RelayClose(object sender, RoutedEventArgs e)
         {
-            serialPort1.Write(new byte[] { 0xFF, 0x01, 0x00 }, 0, 3);
-            panel1.BackColor = Color.Red;
-            label2.Content = "Relay successfully closed!";
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Write(new byte[] { 0xFF, 0x01, 0x00 }, 0, 3);
+                panel1.BackColor = Color.Red;
+                label2.Content = "Relay successfully closed!";
+            }
+
+            else
+            {
+                label2.Content = "The port can not be opened!";
+            }
+
         }
+
+       /* 
+        public int serialBytesToRead()
+        {
+            return serialPort1.BytesToRead;
+        }
+        private string ByteToHex(byte[] data)
+        {
+            StringBuilder sb = new StringBuilder(data.Length * 3);
+            foreach (byte b in data)
+                sb.Append(Convert.ToString(b, 16).PadLeft(2, '0').PadRight(3, ' '));
+            return sb.ToString().ToUpper();
+
+        }*/
 
     }
 
